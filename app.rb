5a1e5c478f3e43e740ce41603575f9bbe160f87b
Apoorv Singh <singhapo@msu.edu>
2015-02-23 11:43:27 -0500

    form with bootstrap

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'holidapi'
 
 class MyWebApp < Sinatra::Base
-	get '/' do
-		time = Time.new
-		params['year'] ||= Time.now.year
-		params['month'] ||= Time.now.month
-		params['day'] ||= Time.now.day
-		@curr_yholiday = HolidApi.get(country: 'us', year: params['year'], month: params['month'], day: params['day'])
-		@b_holiday = HolidApi.get(country: 'us', year: '1994', month: 10)
-		erb :"index"
-	end
+  get '/' do
+    time = Time.new
+    params['year'] ||= Time.now.year
+    params['month'] ||= Time.now.month
+    params['day'] ||= Time.now.day
+    @curr_yholiday = HolidApi.get(country: 'us', year: params['year'], month: params['month'], day: params['day'])
+    @b_holiday = HolidApi.get(country: 'us', year: '1994', month: 10)
+    erb :"index"
+  end
 end
